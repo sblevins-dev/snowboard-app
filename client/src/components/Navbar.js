d6fb6a2f06b86014ff823b8bcba1687bd6d2253a
Stephen Blevins <scblevins27@gmail.com>
2022-03-03 14:34:47 -0500

    added userIcon and hover effects

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,54 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "../css/navbar.css";
-import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { CartContext } from "../contexts/CartContext";
 
 export const Navbar = () => {
   const { cart, isSelected, setIsSelected } = useContext(CartContext);
+  const [userMenu, setUserMenu] = useState(false);
+
+  const handleMouseOver = () => {
+    setUserMenu(true)
+  }
+
+  const handleMouseLeave = () => {
+    setUserMenu(false)
+  }
 
   return (
     <div className="nav-wrapper">
       <div className="logo">SK</div>
       <ul className="nav-links-wrapper">
-        <li className="home links" onClick={() => setIsSelected('home')}>Home</li>
-        <li className="cart links" onClick={() => setIsSelected('cart')}>
-          Cart
-          {cart.length > 0 ? <div className="cart-icon">{cart.length}</div> : <></>}
+        <li className="home links" onClick={() => setIsSelected("home")}>
+          Home
         </li>
-        <li className="about links" onClick={() => setIsSelected('about')}>About</li>
+
+        <li className="cart links" onClick={() => setIsSelected("cart")}>
+          Cart
+          {cart.length > 0 ? (
+            <div className="cart-icon">{cart.length}</div>
+          ) : (
+            <></>
+          )}
+        </li>
+
+        <li className="about links" onClick={() => setIsSelected("about")}>
+          About
+        </li>
+        <li className="user-icon links" onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+          <FaUser />
+          <ul className={!userMenu ? "hidden" : "shown"} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+            <li>
+              Username
+            </li>
+            <li>
+              Account
+            </li>
+            <li className='sign-out'>
+              <FaSignOutAlt /> Sign Out
+            </li>
+          </ul>
+        </li>
       </ul>
     </div>
   );
