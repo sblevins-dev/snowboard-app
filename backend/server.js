6fa4b319155d22ab7533fa5d39dcac1cbabe96bc
Stephen Blevins <scblevins27@gmail.com>
2022-03-08 23:48:07 -0500

    added dirname serve up html

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,32 @@
-const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
-const port = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/products', require('./routes/productRoutes'))
-app.use('/api/users', require('./routes/userRoutes'))
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
-app.use(errorHandler)
+// Serve client
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    )
+  );
+}
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
